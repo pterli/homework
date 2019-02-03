f69673d80e866cb11384c7862d1b6a6acb7891f8
PeterLi <p801106@gmail.com>
2019-02-03 15:25:44 +0800

    first edition of homework

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -1,4 +1,16 @@
 package com.train;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Tester {
+    public static void main(String[] args) {
+        System.out.print("Please enter number of tickets: ");
+        Scanner scanner = new Scanner(System.in);
+        int total_tickets = scanner.nextInt();
+        System.out.print("How many round-trip tickets: ");
+        int round_tickets = scanner.nextInt();
+        Ticket ticket = new Ticket(total_tickets,round_tickets);
+        ticket.calculation();
+    }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -1,2 +1,22 @@
 package com.train
 
+import java.util.*
+const val TicketPrice = 1000
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    print("Please enter number of tickets: ")
+    var totaltickets = scanner.nextInt()
+    print("How many round-trip tickets:")
+    var roundtickets = scanner.nextInt()
+    val result = TicketCalculator(totaltickets, roundtickets)
+    result.calculation()
+}
+
+class TicketCalculator(var totaltickets: Int, var roundtickets: Int){
+    fun calculation(){
+        println("Total Tickets: $totaltickets")
+        println("Round-trip: $roundtickets")
+        println("Total: " + ( (totaltickets - roundtickets) * TicketPrice +roundtickets * TicketPrice * 2 * 0.9))
+    }
+}
